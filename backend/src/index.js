285bf0ec67dd9f2e7b651167fbc85e63cc018d47
Sagar Verma <sagar.verma1195@gmail.com>
2025-12-19 14:24:12 +0530

    Basic structure for REST api

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import app from "./app,js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const startServer = async () => {
+  try {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+startServer();
